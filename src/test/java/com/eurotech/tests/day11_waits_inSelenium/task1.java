1c29b339b8bb0352b7027404082b3b4fdc363006
YAVUZ <yylmz0510@gmail.com>
2023-10-18 21:41:14 +0200

    day10_task

---
+++ src/test/java/com/eurotech/tests/day11_waits_inSelenium/task1.java
@@ -0,0 +1,48 @@
+package com.eurotech.tests.day11_waits_inSelenium;
+
+import com.eurotech.utilities.WebDriverFactory;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class task1 {
+
+    WebDriver driver;
+    WebDriverWait wait;
+
+    @BeforeMethod
+    public void setUp() {
+        driver = WebDriverFactory.getDriver("chrome");
+        //driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        wait = new WebDriverWait(driver, 15);
+    }
+
+    @AfterMethod
+    public void tearDown() throws InterruptedException {
+        Thread.sleep(2000);
+        driver.close();
+    }
+    @Test
+
+    public void timeAlertTask() {
+        driver.get("https://demoqa.com/alerts");
+        driver.findElement(By.xpath("(//button[text()='Click me'])[2]")).click();
+        wait.until(ExpectedConditions.alertIsPresent());
+
+        Alert alert = driver.switchTo().alert();
+        System.out.println("alert.getText() = " + alert.getText());
+        Assert.assertEquals(alert.getText(),"This alert appeared after 5 seconds");
+        alert.accept();
+
+    }
+}
